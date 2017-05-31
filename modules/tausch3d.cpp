586420a6c58443a2df63558717baef5222031db2
Lukas <lukas@photoqt.org>
2017-05-31 09:57:09 -0500

    [Tausch3D] Almost complete 3d version

---
+++ modules/tausch3d.cpp
@@ -0,0 +1,162 @@
+#include "tausch3d.h"
+
+template <class real_t> Tausch3D<real_t>::Tausch3D(int *localDim, int *haloWidth, int numBuffers, int valuesPerPoint, MPI_Comm comm) {
+
+    MPI_Comm_dup(comm, &TAUSCH_COMM);
+
+    // get MPI info
+    MPI_Comm_rank(TAUSCH_COMM, &mpiRank);
+    MPI_Comm_size(TAUSCH_COMM, &mpiSize);
+
+    for(int i = 0; i < 3; ++i)
+        this->localDim[i] = localDim[i];
+
+    for(int i = 0; i < 6; ++i)
+        this->haloWidth[i] = haloWidth[i];
+
+    this->numBuffers = numBuffers;
+    this->valuesPerPoint = valuesPerPoint;
+
+    mpiDatatype = (std::is_same<real_t, float>::value ? MPI_FLOAT
+                        : (std::is_same<real_t, int>::value ? MPI_INT
+                        : (std::is_same<real_t, unsigned int>::value ? MPI_UNSIGNED
+                        : (std::is_same<real_t, long>::value ? MPI_LONG
+                        : (std::is_same<real_t, long long>::value ? MPI_LONG_LONG
+                        : (std::is_same<real_t, long double>::value ? MPI_LONG_DOUBLE : MPI_DOUBLE))))));
+
+}
+
+template <class real_t> Tausch3D<real_t>::~Tausch3D() {
+    for(int i = 0; i < localHaloNumParts; ++i) {
+        delete[] localHaloSpecs[i];
+        delete[] mpiSendBuffer[i];
+        delete[] remoteHaloSpecs[i];
+        delete[] mpiRecvBuffer[i];
+    }
+    delete[] localHaloSpecs;
+    delete[] mpiSendBuffer;
+    delete[] remoteHaloSpecs;
+    delete[] mpiRecvBuffer;
+
+    delete[] mpiSendRequests;
+    delete[] numBuffersPacked;
+    delete[] mpiRecvRequests;
+    delete[] numBuffersUnpacked;
+}
+
+template <class real_t> void Tausch3D<real_t>::setCpuLocalHaloInfo(int numHaloParts, int **haloSpecs) {
+
+    localHaloNumParts = numHaloParts;
+    localHaloSpecs = new int*[numHaloParts];
+    mpiSendBuffer = new real_t*[numHaloParts];
+    mpiSendRequests = new MPI_Request[numHaloParts];
+    numBuffersPacked =  new int[numHaloParts]{};
+
+    for(int i = 0; i < numHaloParts; ++i) {
+
+        localHaloSpecs[i] = new int[8];
+        for(int j = 0; j < 8; ++j)
+            localHaloSpecs[i][j] = haloSpecs[i][j];
+
+        int size = numBuffers*valuesPerPoint*haloSpecs[i][3]*haloSpecs[i][4]*haloSpecs[i][5];
+
+        mpiSendBuffer[i] = new real_t[size]{};
+        MPI_Send_init(&mpiSendBuffer[i][0], size, mpiDatatype, haloSpecs[i][6], haloSpecs[i][7], TAUSCH_COMM, &mpiSendRequests[i]);
+
+    }
+
+}
+
+template <class real_t> void Tausch3D<real_t>::setCpuRemoteHaloInfo(int numHaloParts, int **haloSpecs) {
+
+    remoteHaloNumParts = numHaloParts;
+    remoteHaloSpecs = new int*[numHaloParts];
+    mpiRecvBuffer = new real_t*[numHaloParts];
+    mpiRecvRequests = new MPI_Request[numHaloParts];
+    numBuffersUnpacked =  new int[numHaloParts]{};
+
+    for(int i = 0; i < numHaloParts; ++i) {
+
+        remoteHaloSpecs[i] = new int[8];
+        for(int j = 0; j < 8; ++j)
+            remoteHaloSpecs[i][j] = haloSpecs[i][j];
+
+        int size = numBuffers*valuesPerPoint*haloSpecs[i][3]*haloSpecs[i][4]*haloSpecs[i][5];
+
+        mpiRecvBuffer[i] = new real_t[size]{};
+        MPI_Recv_init(&mpiRecvBuffer[i][0], size, mpiDatatype, haloSpecs[i][6], haloSpecs[i][7], TAUSCH_COMM, &mpiRecvRequests[i]);
+
+    }
+
+}
+
+template <class real_t> void Tausch3D<real_t>::postMpiReceives() {
+
+    for(int rem = 0; rem < remoteHaloNumParts; ++rem)
+        MPI_Start(&mpiRecvRequests[rem]);
+
+}
+
+template <class real_t> void Tausch3D<real_t>::packNextSendBuffer(int id, real_t *buf) {
+
+    if(numBuffersPacked[id] == numBuffers)
+        numBuffersPacked[id] = 0;
+
+    int size = localHaloSpecs[id][3] * localHaloSpecs[id][4] * localHaloSpecs[id][5];
+    for(int s = 0; s < size; ++s) {
+//        int index = (s/localHaloSpecs[id][2] + localHaloSpecs[id][1])*(localDim[TAUSCH_X] + haloWidth[TAUSCH_LEFT] + haloWidth[TAUSCH_RIGHT]) +
+//                    s%localHaloSpecs[id][2] + localHaloSpecs[id][0];
+//        for(int val = 0; val < valuesPerPoint; ++val)
+//            mpiSendBuffer[id][numBuffersPacked[id]*valuesPerPoint*size + valuesPerPoint*s + val] = buf[valuesPerPoint*index + val];
+    }
+    ++numBuffersPacked[id];
+
+}
+
+template <class real_t> void Tausch3D<real_t>::send(int id) {
+
+    if(numBuffersPacked[id] != numBuffers) {
+        std::cerr << "Tausch3D: ERROR: halo part " << id << " has " << numBuffersPacked[id] << " out of "
+                  << numBuffers << " send buffers packed... Abort!" << std::endl;
+        exit(1);
+    }
+
+    MPI_Start(&mpiSendRequests[id]);
+
+}
+
+template <class real_t> void Tausch3D<real_t>::recv(int id) {
+    numBuffersUnpacked[id] = 0;
+    MPI_Wait(&mpiRecvRequests[id], MPI_STATUS_IGNORE);
+}
+
+template <class real_t> void Tausch3D<real_t>::unpackNextRecvBuffer(int id, real_t *buf) {
+
+    int size = remoteHaloSpecs[id][3] * remoteHaloSpecs[id][4] * localHaloSpecs[id][5];
+    for(int s = 0; s < size; ++s) {
+//        int index = (s/remoteHaloSpecs[id][2] + remoteHaloSpecs[id][1])*(localDim[TAUSCH_X] + haloWidth[TAUSCH_LEFT] + haloWidth[TAUSCH_RIGHT]) +
+//                    s%remoteHaloSpecs[id][2] + remoteHaloSpecs[id][0];
+//        for(int val = 0; val < valuesPerPoint; ++val)
+//            buf[valuesPerPoint*index + val] = mpiRecvBuffer[id][numBuffersUnpacked[id]*valuesPerPoint*size + valuesPerPoint*s + val];
+    }
+    ++numBuffersUnpacked[id];
+
+}
+
+template <class real_t> void Tausch3D<real_t>::packAndSend(int id, real_t *buf) {
+    packNextSendBuffer(id, buf);
+    send(id);
+}
+
+template <class real_t> void Tausch3D<real_t>::recvAndUnpack(int id, real_t *buf) {
+    recv(id);
+    unpackNextRecvBuffer(id, buf);
+}
+
+template class Tausch3D<double>;
+template class Tausch3D<float>;
+template class Tausch3D<int>;
+template class Tausch3D<unsigned int>;
+template class Tausch3D<long>;
+template class Tausch3D<long long>;
+template class Tausch3D<long double>;
--- modules/tausch3d.h
+++ modules/tausch3d.h
@@ -13,18 +13,175 @@
 
 #ifndef TAUSCH_ENUM
 #define TAUSCH_ENUM
-namespace Tausch {
-    /*!
-     * These are the edges available for inter-MPI halo exchanges: LEFT, RIGHT, TOP, BOTTOM.
-     */
-    enum Edges { LEFT, RIGHT, TOP, BOTTOM };
+/*!
+ * These are the edges available for inter-MPI halo exchanges: LEFT, RIGHT, TOP, BOTTOM.
+ */
+enum Edges { TAUSCH_LEFT, TAUSCH_RIGHT, TAUSCH_TOP, TAUSCH_BOTTOM, TAUSCH_FRONT, TAUSCH_BACK };
 
-    /*!
-     * These are the two dimensions used, used for clarity as to which array entry is which dimension: X, Y.
-     */
-    enum Dimensions { X, Y, Z };
-
-}
+/*!
+ * These are the two dimensions used, used for clarity as to which array entry is which dimension: X, Y.
+ */
+enum Dimensions { TAUSCH_X, TAUSCH_Y, TAUSCH_Z };
 #endif // TAUSCH_ENUM
+
+/*!
+ *
+ * \brief
+ *  A library providing a clean and efficient interface for halo exchange in two dimensions.
+ *
+ * %Tausch3D is a library that provides a clean and efficient C and C++ API for halo exchange for three dimensional domains. It doesn't assume
+ * anything about the grid, except that the data is stored in one contiguous buffer. After specifying the local and remote halo regions, it takes care
+ * of extracting the data for each halo from as many buffers as there are, sends the data for each halo combined into a single message each, and
+ * unpacks the received data into the same number of buffers again. This is a template class and can be used with any of the following data types:
+ * double, float, int, unsigned int, long, long long, long double.
+ */
+template <class real_t>
+class Tausch3D : public Tausch<real_t> {
+
+public:
+
+    /*!
+     * The constructor, initiating the 3D Tausch object.
+     *
+     * \param localDim
+     *  Array of size 3 holding the dimensions of the local partition (not the global dimensions), with the x dimension being the first value, the
+     *  y dimension being the second one, and the z dimension being the last one.
+     * \param haloWidth
+     *  Array of size 6 containing the widths of the CPU-to-CPU halos, i.e., the inter-MPI halo. The order in which the halo widths are expected to be
+     *  stored is: LEFT -> RIGHT -> TOP -> BOTTOM -> FRONT -> BACK
+     * \param numBuffers
+     *  The number of buffers that will be used. If more than one, they are all combined into one message. All buffers will have to use the same
+     *  discretisation! Default value: 1
+     * \param valuesPerPoint
+     *  How many values are stored consecutively per point in the same buffer. All points will have to have the same number of values stored for them.
+     *  Default value: 1
+     * \param comm
+     *  The MPI Communictor to be used. %Tausch3D will duplicate the communicator, thus it is safe to have multiple instances of %Tausch3D working
+     *  with the same communicator. By default, MPI_COMM_WORLD will be used.
+     */
+    Tausch3D(int *localDim, int *haloWidth, int numBuffers = 1, int valuesPerPoint = 1, MPI_Comm comm = MPI_COMM_WORLD);
+
+    /*!
+     * The destructor cleaning up all memory.
+     */
+    ~Tausch3D();
+
+    /*!
+     * Set the info about all local halos that need to be sent to remote MPI ranks.
+     * \param numHaloParts
+     *  How many different parts there are to the halo
+     * \param haloSpecs
+     *  The specification of the different halo parts. This is expected to be a an array of arrays of int's. Each array of int's contains 8 entries,
+     *  the order of which will be preserved, and each halo region can be referenced later by its index in this array. The 8 entries are:
+     *   1. The starting x coordinate of the local region
+     *   2. The starting y coordinate of the local region
+     *   3. The starting z coordinate of the local region
+     *   4. The width of the region
+     *   5. The height of the region
+     *   6. The depth of the region
+     *   7. The receiving processor
+     *   8. A unique id that matches the id for the corresponding local halo region of the sending MPI rank.
+     */
+    void setCpuLocalHaloInfo(int numHaloParts, int **haloSpecs);
+
+    /*!
+     * Set the info about all remote halos that are needed by this MPI rank.
+     * \param numHaloParts
+     *  How many different parts there are to the halo
+     * \param haloSpecs
+     *  The specification of the different halo parts. This is expected to be a an array of arrays of int's. Each array of int's contains 8 entries,
+     *  the order of which will be preserved, and each halo region can be referenced later by its index in this array. The 8 entries ares:
+     *   1. The starting x coordinate of the halo region
+     *   2. The starting y coordinate of the halo region
+     *   3. The starting z coordinate of the halo region
+     *   4. The width of the halo region
+     *   5. The height of the halo region
+     *   6. The depth of the halo region
+     *   7. The sending processor
+     *   8. A unique id that matches the id for the corresponding remote halo region of the receiving MPI rank.
+     */
+    void setCpuRemoteHaloInfo(int numHaloParts, int **haloSpecs);
+
+    /*!
+     * Post all receives for the current MPI rank. This doesn't do anything else but call MPI_Start() and all MPI_Recv_init().
+     */
+    void postMpiReceives();
+
+    /*!
+     * This packs the next buffer for a send. This has to be called as many times as there are buffers before sending the message.
+     * \param id
+     *  The id of the halo region. This is the index of this halo region in the local halo specification provided with setLocalHaloInfo().
+     * \param buf
+     *  The buffer from which the data is to be extracted according to the local halo specification.
+     */
+    void packNextSendBuffer(int id, real_t *buf);
+
+    /*!
+     * Sends off the send buffer for the specified halo region. This calls MPI_Start() on the respective MPI_Send_init().
+     * \param id
+     *  The id of the halo region. This is the index of this halo region in the local halo specification provided with setLocalHaloInfo().
+     */
+    void send(int id);
+
+    /*!
+     * Makes sure the MPI message for the specified halo is received by this buffer. It does not do anything with that message!
+     * \param id
+     *  The id of the halo region. This is the index of this halo region in the remote halo specification provided with setRemoteHaloInfo().
+     */
+    void recv(int id);
+
+    /*!
+     * This unpacks the next halo from the received message into the provided buffer. This has to be called as many times as there are buffers.
+     * \param id
+     *  The id of the halo region. This is the index of this halo region in the remote halo specification provided with setRemoteHaloInfo().
+     * \param[out] buf
+     *  The buffer to which the extracted data is to be written to according to the remote halo specification
+     */
+    void unpackNextRecvBuffer(int id, real_t *buf);
+
+    /*!
+     * Shortcut function. If only one buffer is used, this will both pack the data out of the provided buffer and send it off, all with one call.
+     * \param id
+     *  The id of the halo region. This is the index of this halo region in the local halo specification provided with setLocalHaloInfo().
+     * \param buf
+     *  The buffer from which the data is to be extracted according to the local halo specification.
+     */
+    void packAndSend(int id, real_t *buf);
+    /*!
+     * Shortcut function. If only one buffer is used, this will both receive the MPI message and unpack the received data into the provided buffer,
+     * all with one call.
+     * \param id
+     *  The id of the halo region. This is the index of this halo region in the remote halo specification provided with setRemoteHaloInfo().
+     * \param[out] buf
+     *  The buffer to which the extracted data is to be written to according to the remote halo specification
+     */
+    void recvAndUnpack(int id, real_t *buf);
+
+private:
+
+    int localDim[3], haloWidth[6];
+
+    MPI_Comm TAUSCH_COMM;
+    int mpiRank, mpiSize;
+
+    int localHaloNumParts;
+    int **localHaloSpecs;
+    int remoteHaloNumParts;
+    int **remoteHaloSpecs;
+
+    int numBuffers;
+    int valuesPerPoint;
+
+    real_t **mpiRecvBuffer;
+    real_t **mpiSendBuffer;
+    MPI_Request *mpiRecvRequests;
+    MPI_Request *mpiSendRequests;
+    MPI_Datatype mpiDatatype;
+
+    int *numBuffersPacked;
+    int *numBuffersUnpacked;
+
+};
+
 
 #endif // TAUSCH3D_H
