62385b7e63e12882bd1a9a412e6dbb92c318c574
Lukas Spies <lspies@illinois.edu>
2019-01-09 09:50:07 -0600

    [c2g/g2c] adjust names of testing functions

--- testing/c2g.cpp
+++ testing/c2g.cpp
@@ -11,7 +11,7 @@ static cl::CommandQueue tauschcl_queue;
 
 void setupOpenCL();
 
-int testC2C_1buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double *expected) {
+int testC2G_1buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double *expected) {
 
     Tausch<double> *tausch = new Tausch<double>(tauschcl_device, tauschcl_context, tauschcl_queue, "double",
                                                 MPI_DOUBLE, MPI_COMM_WORLD, true);
@@ -47,7 +47,7 @@ int testC2C_1buf(std::vector<int> sendIndices, std::vector<int> recvIndices, dou
 
 }
 
-int testC2C_2buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double *expected1, double *expected2) {
+int testC2G_2buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double *expected1, double *expected2) {
 
     Tausch<double> *tausch = new Tausch<double>(tauschcl_device, tauschcl_context, tauschcl_queue, "double",
                                                 MPI_DOUBLE, MPI_COMM_WORLD, true);
@@ -117,22 +117,22 @@ TEST_CASE("1 buffer data exchange") {
     std::vector<int> sendIndices1 = {0,1,2,3,4};
     std::vector<int> recvIndices1 = {5,6,7,8,9};
     double *expected1 = new double[10]{1,2,3,4,5,1,2,3,4,5};
-    REQUIRE(testC2C_1buf(sendIndices1, recvIndices1, expected1) == 0);
+    REQUIRE(testC2G_1buf(sendIndices1, recvIndices1, expected1) == 0);
 
     std::vector<int> sendIndices2 = {0,2,4,6,8};
     std::vector<int> recvIndices2 = {1,3,5,7,9};
     double *expected2 = new double[10]{1,1,3,3,5,5,7,7,9,9};
-    REQUIRE(testC2C_1buf(sendIndices2, recvIndices2, expected2) == 0);
+    REQUIRE(testC2G_1buf(sendIndices2, recvIndices2, expected2) == 0);
 
     std::vector<int> sendIndices3 = {0,2,4,7};
     std::vector<int> recvIndices3 = {0,2,4,7};
     double *expected3 = new double[10]{1,2,3,4,5,6,7,8,9,10};
-    REQUIRE(testC2C_1buf(sendIndices3, recvIndices3, expected3) == 0);
+    REQUIRE(testC2G_1buf(sendIndices3, recvIndices3, expected3) == 0);
 
     std::vector<int> sendIndices4 = {};
     std::vector<int> recvIndices4 = {};
     double *expected4 = new double[10]{1,2,3,4,5,6,7,8,9,10};
-    REQUIRE(testC2C_1buf(sendIndices4, recvIndices4, expected4) == 0);
+    REQUIRE(testC2G_1buf(sendIndices4, recvIndices4, expected4) == 0);
 
     delete[] expected1;
     delete[] expected2;
@@ -147,25 +147,25 @@ TEST_CASE("2 buffer data exchange") {
     std::vector<int> recvIndices1 = {5,6,7,8,9};
     double *expected1_1 = new double[10]{1,2,3,4,5,11,12,13,14,15};
     double *expected1_2 = new double[10]{11,12,13,14,15,1,2,3,4,5};
-    REQUIRE(testC2C_2buf(sendIndices1, recvIndices1, expected1_1, expected1_2) == 0);
+    REQUIRE(testC2G_2buf(sendIndices1, recvIndices1, expected1_1, expected1_2) == 0);
 
     std::vector<int> sendIndices2 = {0,2,4,6,8};
     std::vector<int> recvIndices2 = {1,3,5,7,9};
     double *expected2_1 = new double[10]{1,11,3,13,5,15,7,17,9,19};
     double *expected2_2 = new double[10]{11,1,13,3,15,5,17,7,19,9};
-    REQUIRE(testC2C_2buf(sendIndices2, recvIndices2, expected2_1, expected2_2) == 0);
+    REQUIRE(testC2G_2buf(sendIndices2, recvIndices2, expected2_1, expected2_2) == 0);
 
     std::vector<int> sendIndices3 = {0,2,4,7};
     std::vector<int> recvIndices3 = {0,2,4,7};
     double *expected3_1 = new double[10]{11,2,13,4,15,6,7,18,9,10};
     double *expected3_2 = new double[10]{1,12,3,14,5,16,17,8,19,20};
-    REQUIRE(testC2C_2buf(sendIndices3, recvIndices3, expected3_1, expected3_2) == 0);
+    REQUIRE(testC2G_2buf(sendIndices3, recvIndices3, expected3_1, expected3_2) == 0);
 
     std::vector<int> sendIndices4 = {};
     std::vector<int> recvIndices4 = {};
     double *expected4_1 = new double[10]{1,2,3,4,5,6,7,8,9,10};
     double *expected4_2 = new double[10]{11,12,13,14,15,16,17,18,19,20};
-    REQUIRE(testC2C_2buf(sendIndices4, recvIndices4, expected4_1, expected4_2) == 0);
+    REQUIRE(testC2G_2buf(sendIndices4, recvIndices4, expected4_1, expected4_2) == 0);
 
     delete[] expected1_1;
     delete[] expected1_2;
