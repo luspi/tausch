aacfa51e64ba15a32161604454293bba7e5a07a4
Lukas Spies <lspies@illinois.edu>
2019-06-10 16:25:01 -0500

    update cpu2cpu tests to test for derived datatype and 'normal' communication

--- testing/cpu2cpu.cpp
+++ testing/cpu2cpu.cpp
@@ -4,7 +4,7 @@
 #include <mpi.h>
 #include "../tausch.h"
 
-int test_1buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double *expected) {
+int test_1buf(std::vector<int> sendIndices, std::vector<int> recvIndices, std::vector<int> expected, bool useDerivedMpiDatatype) {
 
     Tausch<double> *tausch = new Tausch<double>(MPI_DOUBLE, MPI_COMM_WORLD, true);
 
@@ -12,15 +12,27 @@ int test_1buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double
     for(int i = 0; i < 10; ++i)
         buf1[i] = i + 1;
 
-    tausch->addLocalHaloInfo(sendIndices);
-    tausch->addRemoteHaloInfo(recvIndices);
+    if(useDerivedMpiDatatype) {
 
-    tausch->packSendBuffer(0, 0, buf1);
+        tausch->addLocalHaloInfo(sendIndices, 1, -1, TauschOptimizationHint::UseMpiDerivedDatatype);
+        tausch->addRemoteHaloInfo(recvIndices, 1, -1, TauschOptimizationHint::UseMpiDerivedDatatype);
 
-    tausch->send(0, 0, 0);
-    tausch->recv(0, 0, 0);
+        tausch->send(0, 0, 0, buf1);
+        tausch->recv(0, 0, 0, buf1);
 
-    tausch->unpackRecvBuffer(0, 0, buf1);
+    } else {
+
+        tausch->addLocalHaloInfo(sendIndices, 1);
+        tausch->addRemoteHaloInfo(recvIndices, 1);
+
+        tausch->packSendBuffer(0, 0, buf1);
+
+        tausch->send(0, 0, 0);
+        tausch->recv(0, 0, 0);
+
+        tausch->unpackRecvBuffer(0, 0, buf1);
+
+    }
 
     delete tausch;
 
@@ -35,7 +47,7 @@ int test_1buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double
 
 }
 
-int test_2buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double *expected1, double *expected2) {
+int test_2buf(std::vector<int> sendIndices, std::vector<int> recvIndices, std::vector<int> expected1, std::vector<int> expected2, bool useDerivedMpiDatatype) {
 
     Tausch<double> *tausch = new Tausch<double>(MPI_DOUBLE, MPI_COMM_WORLD, true);
 
@@ -46,17 +58,32 @@ int test_2buf(std::vector<int> sendIndices, std::vector<int> recvIndices, double
         buf2[i] = i + 11;
     }
 
-    tausch->addLocalHaloInfo(sendIndices, 2);
-    tausch->addRemoteHaloInfo(recvIndices, 2);
+    if(useDerivedMpiDatatype) {
 
-    tausch->packSendBuffer(0, 0, buf1);
-    tausch->packSendBuffer(0, 1, buf2);
+        tausch->addLocalHaloInfo(sendIndices, 2, -1, TauschOptimizationHint::UseMpiDerivedDatatype);
+        tausch->addRemoteHaloInfo(recvIndices, 2, -1, TauschOptimizationHint::UseMpiDerivedDatatype);
 
-    tausch->send(0, 0, 0);
-    tausch->recv(0, 0, 0);
+        tausch->send(0, 0, 0, buf1);
+        tausch->send(0, 1, 0, buf2);
+        tausch->recv(0, 0, 0, buf2);
+        tausch->recv(0, 1, 0, buf1);
 
-    tausch->unpackRecvBuffer(0, 0, buf2);
-    tausch->unpackRecvBuffer(0, 1, buf1);
+
+    } else {
+
+        tausch->addLocalHaloInfo(sendIndices, 2);
+        tausch->addRemoteHaloInfo(recvIndices, 2);
+
+        tausch->packSendBuffer(0, 0, buf1);
+        tausch->packSendBuffer(0, 1, buf2);
+
+        tausch->send(0, 0, 0);
+        tausch->recv(0, 0, 0);
+
+        tausch->unpackRecvBuffer(0, 0, buf2);
+        tausch->unpackRecvBuffer(0, 1, buf1);
+
+    }
 
     delete tausch;
 
@@ -90,34 +117,43 @@ TEST_CASE("1 buffer data exchange") {
 
     std::vector<int> sendIndices1 = {0,1,2,3,4};
     std::vector<int> recvIndices1 = {5,6,7,8,9};
-    double *expected1 = new double[10]{1,2,3,4,5,1,2,3,4,5};
-    REQUIRE(test_1buf(sendIndices1, recvIndices1, expected1) == 0);
+    std::vector<int> expected1 = {1,2,3,4,5,1,2,3,4,5};
+    REQUIRE(test_1buf(sendIndices1, recvIndices1, expected1, false) == 0);
+    sendIndices1 = {0,1,2,3,4};
+    recvIndices1 = {5,6,7,8,9};
+    REQUIRE(test_1buf(sendIndices1, recvIndices1, expected1, true) == 0);
 
     std::vector<int> sendIndices2 = {0,2,4,6,8};
     std::vector<int> recvIndices2 = {1,3,5,7,9};
-    double *expected2 = new double[10]{1,1,3,3,5,5,7,7,9,9};
-    REQUIRE(test_1buf(sendIndices2, recvIndices2, expected2) == 0);
+    std::vector<int> expected2 = {1,1,3,3,5,5,7,7,9,9};
+    REQUIRE(test_1buf(sendIndices2, recvIndices2, expected2, false) == 0);
+    sendIndices2 = {0,2,4,6,8};
+    recvIndices2 = {1,3,5,7,9};
+    REQUIRE(test_1buf(sendIndices2, recvIndices2, expected2, true) == 0);
 
     std::vector<int> sendIndices3 = {0,2,4,7};
     std::vector<int> recvIndices3 = {0,2,4,7};
-    double *expected3 = new double[10]{1,2,3,4,5,6,7,8,9,10};
-    REQUIRE(test_1buf(sendIndices3, recvIndices3, expected3) == 0);
+    std::vector<int> expected3 = {1,2,3,4,5,6,7,8,9,10};
+    REQUIRE(test_1buf(sendIndices3, recvIndices3, expected3, false) == 0);
+    sendIndices3 = {0,2,4,7};
+    recvIndices3 = {0,2,4,7};
+    REQUIRE(test_1buf(sendIndices3, recvIndices3, expected3, true) == 0);
 
     std::vector<int> sendIndices4 = {};
     std::vector<int> recvIndices4 = {};
-    double *expected4 = new double[10]{1,2,3,4,5,6,7,8,9,10};
-    REQUIRE(test_1buf(sendIndices4, recvIndices4, expected4) == 0);
+    std::vector<int> expected4 = {1,2,3,4,5,6,7,8,9,10};
+    REQUIRE(test_1buf(sendIndices4, recvIndices4, expected4, false) == 0);
+    sendIndices4 = {};
+    recvIndices4 = {};
+    REQUIRE(test_1buf(sendIndices4, recvIndices4, expected4, true) == 0);
 
     std::vector<int> sendIndices5 = {0,3,4,6,7};
     std::vector<int> recvIndices5 = {1,2,3,4,5};
-    double *expected5 = new double[10]{1,1,4,5,7,8,7,8,9,10};
-    REQUIRE(test_1buf(sendIndices5, recvIndices5, expected5) == 0);
-
-    delete[] expected1;
-    delete[] expected2;
-    delete[] expected3;
-    delete[] expected4;
-    delete[] expected5;
+    std::vector<int> expected5 = {1,1,4,5,7,8,7,8,9,10};
+    REQUIRE(test_1buf(sendIndices5, recvIndices5, expected5, false) == 0);
+    sendIndices5 = {0,3,4,6,7};
+    recvIndices5 = {1,2,3,4,5};
+    REQUIRE(test_1buf(sendIndices5, recvIndices5, expected5, true) == 0);
 
 }
 
@@ -125,44 +161,48 @@ TEST_CASE("2 buffer data exchange") {
 
     std::vector<int> sendIndices1 = {0,1,2,3,4};
     std::vector<int> recvIndices1 = {5,6,7,8,9};
-    double *expected1_1 = new double[10]{1,2,3,4,5,11,12,13,14,15};
-    double *expected1_2 = new double[10]{11,12,13,14,15,1,2,3,4,5};
-    REQUIRE(test_2buf(sendIndices1, recvIndices1, expected1_1, expected1_2) == 0);
+    std::vector<int> expected1_1 = {1,2,3,4,5,11,12,13,14,15};
+    std::vector<int> expected1_2 = {11,12,13,14,15,1,2,3,4,5};
+    REQUIRE(test_2buf(sendIndices1, recvIndices1, expected1_1, expected1_2, false) == 0);
+    sendIndices1 = {0,1,2,3,4};
+    recvIndices1 = {5,6,7,8,9};
+    REQUIRE(test_2buf(sendIndices1, recvIndices1, expected1_1, expected1_2, true) == 0);
 
     std::vector<int> sendIndices2 = {0,2,4,6,8};
     std::vector<int> recvIndices2 = {1,3,5,7,9};
-    double *expected2_1 = new double[10]{1,11,3,13,5,15,7,17,9,19};
-    double *expected2_2 = new double[10]{11,1,13,3,15,5,17,7,19,9};
-    REQUIRE(test_2buf(sendIndices2, recvIndices2, expected2_1, expected2_2) == 0);
+    std::vector<int> expected2_1 = {1,11,3,13,5,15,7,17,9,19};
+    std::vector<int> expected2_2 = {11,1,13,3,15,5,17,7,19,9};
+    REQUIRE(test_2buf(sendIndices2, recvIndices2, expected2_1, expected2_2, false) == 0);
+    sendIndices2 = {0,2,4,6,8};
+    recvIndices2 = {1,3,5,7,9};
+    REQUIRE(test_2buf(sendIndices2, recvIndices2, expected2_1, expected2_2, true) == 0);
 
     std::vector<int> sendIndices3 = {0,2,4,7};
     std::vector<int> recvIndices3 = {0,2,4,7};
-    double *expected3_1 = new double[10]{11,2,13,4,15,6,7,18,9,10};
-    double *expected3_2 = new double[10]{1,12,3,14,5,16,17,8,19,20};
-    REQUIRE(test_2buf(sendIndices3, recvIndices3, expected3_1, expected3_2) == 0);
+    std::vector<int> expected3_1 = {11,2,13,4,15,6,7,18,9,10};
+    std::vector<int> expected3_2 = {1,12,3,14,5,16,17,8,19,20};
+    REQUIRE(test_2buf(sendIndices3, recvIndices3, expected3_1, expected3_2, false) == 0);
+    sendIndices3 = {0,2,4,7};
+    recvIndices3 = {0,2,4,7};
+    REQUIRE(test_2buf(sendIndices3, recvIndices3, expected3_1, expected3_2, true) == 0);
 
     std::vector<int> sendIndices4 = {};
     std::vector<int> recvIndices4 = {};
-    double *expected4_1 = new double[10]{1,2,3,4,5,6,7,8,9,10};
-    double *expected4_2 = new double[10]{11,12,13,14,15,16,17,18,19,20};
-    REQUIRE(test_2buf(sendIndices4, recvIndices4, expected4_1, expected4_2) == 0);
+    std::vector<int> expected4_1 = {1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> expected4_2 = {11,12,13,14,15,16,17,18,19,20};
+    REQUIRE(test_2buf(sendIndices4, recvIndices4, expected4_1, expected4_2, false) == 0);
+    sendIndices4 = {};
+    recvIndices4 = {};
+    REQUIRE(test_2buf(sendIndices4, recvIndices4, expected4_1, expected4_2, true) == 0);
 
     std::vector<int> sendIndices5 = {0,3,4,6,7};
     std::vector<int> recvIndices5 = {1,2,3,4,5};
-    double *expected5_1 = new double[10]{1,11,14,15,17,18,7,8,9,10};
-    double *expected5_2 = new double[10]{11,1,4,5,7,8,17,18,19,20};
-    REQUIRE(test_2buf(sendIndices5, recvIndices5, expected5_1, expected5_2) == 0);
-
-    delete[] expected1_1;
-    delete[] expected1_2;
-    delete[] expected2_1;
-    delete[] expected2_2;
-    delete[] expected3_1;
-    delete[] expected3_2;
-    delete[] expected4_1;
-    delete[] expected4_2;
-    delete[] expected5_1;
-    delete[] expected5_2;
+    std::vector<int> expected5_1 = {1,11,14,15,17,18,7,8,9,10};
+    std::vector<int> expected5_2 = {11,1,4,5,7,8,17,18,19,20};
+    REQUIRE(test_2buf(sendIndices5, recvIndices5, expected5_1, expected5_2, false) == 0);
+    sendIndices5 = {0,3,4,6,7};
+    recvIndices5 = {1,2,3,4,5};
+    REQUIRE(test_2buf(sendIndices5, recvIndices5, expected5_1, expected5_2, true) == 0);
 
 }
 
